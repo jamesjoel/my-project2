ec7ebc6f8ff3d7395a47c0df7e99a44eafd71949
jamesjoel <jamesjoelj@gmail.com>
2023-06-26 17:25:43 +0530

    old folder merge into here

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const root = require("path").join(__dirname, "build");
+const routes = require("./config/routes");
+const upload = require("express-fileupload");
 
-app.use(cors());
 app.use(express.static(root));
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
+app.use(cors());
+app.use(upload());
+
+app.use(routes);
 
 app.get("*", (req, res)=>{
     res.sendFile(__dirname+"/index.html");
